c6c983aa48761ee574964829f4c35217495f5dd4
Jordan Mahsman <31456960+Mahsman27@users.noreply.github.com>
2019-09-01 21:18:22 -0500

    Simple print statements

---
+++ Print.py
@@ -0,0 +1,14 @@
+print('Hello World')
+print('Jordan Mahsman')
+
+#print using concatenator
+first_name = 'Jordan'
+last_name = 'Mahsman'
+print(first_name + ' ' + last_name)
+
+#print using lists
+list1 = [1,2,3,4,5]
+print(*list1)
+
+list2 = ['Bob', 'George', 'Larry', 'Robert']
+print(*list2)
